88c8ac1925e52d4aeba823f62fdc32f374d43186
gionaico <gmc.yanez@gmail.com>
2025-04-11 19:36:47 +0200

    frontend backend working

--- backend-chat/main.py
+++ backend-chat/main.py
@@ -5,6 +5,8 @@ from model1 import responseFromModel  # Importar la función desde utils.py
 import pickle
 from transformers import GPT2LMHeadModel, AutoTokenizer
 import os
+from fastapi.middleware.cors import CORSMiddleware
+
 import torch
 
 
@@ -27,31 +29,31 @@ device = torch.device("cpu")
 model3.to(device)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas las URLs
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los headers
+)
 class InputText(BaseModel):
   text: str
   
-# Modelo de datos para recibir los parámetros
-class Item(BaseModel):
-    name: str
-    #price: float
-    #quantity: int
 
 # Endpoint que recibe datos por POST y devuelve un objeto
 #curl -X 'POST' 'http://127.0.0.1:8000/model1/' -H 'Content-Type: application/json' -d '{"name": "Laptop", "price": 1200.50, "quantity": 2}'
 #curl -X 'POST' 'http://127.0.0.1:8000/model1/' -H 'Content-Type: application/json' -d '{"name": "Las tortugas son animales mamiferos que viv"}'
-@app.post("/model1/")
-async def create_item(item: Item):
-  pruebatext = await responseFromModel(item.name)
-  print("--------------"+pruebatext+"--------------")
+@app.post("/model0/")
+async def create_item(item: InputText):
+  response = await responseFromModel(item.text)
+  print("--------------"+response+"--------------")
   return {
-    "message": "Ítem recibido",
-    "data": item,
-    "responseFromModel": pruebatext
+    "response": response
   }
   
   
 #curl -X 'POST' 'http://127.0.0.1:8000/model2/' -H 'Content-Type: application/json' -d '{"text": "Los hábitos alimenticios de las tortugas de caja orientales puede variar mucho debido al gusto de cada una, la temperatura, la iluminación y su entorno. A diferencia de los animales de sangre caliente su metabolismo no maneja el hambre, solo puede disminuir su nivel de actividad, retirarse a sus caparazones y detener el consumo de alimentos hasta que surjan mejores condiciones. En la naturaleza las tortugas de caja del este son oportunistas omnívoras y se alimentan de una variedad de materia animal y vegetal. Hay una variedad de alimentos que son universalmente aceptados por las tortugas de caja del este, que incluyen gusanos, caracoles, escarabajos, orugas, hierbas, fruta caída, bayas, hongos, flores, el pasto de malas hierbas, y el carrión. Estudios en los humedales de la bahía de Maryland también han demostrado que las tortugas de caja del este se han alimentado de aves vivas que estaban atrapadas en redes. Muchas veces, van a comer un trozo de comida, sobre todo en cautividad, solo porque ven y huelen a comestibles, como hamburguesas o huevos, aunque el alimento puede ser nocivo o no saludable. También se sabe que han consumido hongos tóxicos. La evidencia anecdótica sugiere que los recién nacidos de tortugas de caja son más carnívoros que sus subadultos y adultos. No hay todavía ninguna evidencia concreta para apoyar esta teoría."}'
-@app.post("/model2/")
+@app.post("/model1/")
 async def create_item(input: InputText):
   
   X_nuevos = vectorizador2.transform([input.text])
@@ -64,12 +66,12 @@ async def create_item(input: InputText):
     3:"mating",
   }
   
-  return "Este texto es de tipo: " + categories[predicciones[0]]
+  return {"response": "Este texto es de tipo: " + categories[predicciones[0]]}
 
 
 
 #curl -X 'POST' 'http://127.0.0.1:8000/model3/' -H 'Content-Type: application/json' -d '{"text": "¿ cual es el habitat de los mamiferos ?"}'
-@app.post("/model3/")
+@app.post("/model2/")
 async def create_item(input: InputText):
   model3.eval()
   generated = torch.tensor(tokenizer.encode(input.text)).unsqueeze(0)
@@ -86,6 +88,6 @@ async def create_item(input: InputText):
   
   print("-------"+respuesta+"-------")  
   
-  return respuesta
+  return {"response":respuesta}
 
 
--- frontend/app/welcome/welcome.tsx
+++ frontend/app/welcome/welcome.tsx
@@ -1,18 +1,25 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 import React, { useState } from "react";
+import { FaUserSecret } from "react-icons/fa6";
+import { LiaUserSecretSolid } from "react-icons/lia";
+
+import { SiProbot } from "react-icons/si";
+import { FaRobot } from "react-icons/fa";
+import { GiRobotAntennas } from "react-icons/gi";
 
 const MessageArea = ({ onSendMessage, ModelsState, setModelsState }: any) => {
-  const [message, setMessage] = useState("");
+  const modelSelected = ModelsState?.find((item: any) => item.selected);
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    const index = ModelsState.find((element: any) => element.selected);
+    const data = ModelsState.find((element: any) => element.selected);
+    //const index = ModelsState.findIndex((element: any) => element.id === value);
 
-    const obj = ModelsState?.[index];
+    if (!data.inputText?.trim()) return alert("el input esta vacio");
 
-    if (!obj.inputText?.trim()) return alert("el input esta vacio");
+    const index = parseInt(data?.id);
 
     setModelsState((prevItems: any) =>
       prevItems.map((item: any, i: number) => {
@@ -22,14 +29,44 @@ const MessageArea = ({ onSendMessage, ModelsState, setModelsState }: any) => {
                 ...item,
                 mesagges: [
                   ...item.mesagges,
-                  { sender: "chat", text: item.inputText },
+                  { sender: "user", text: item.inputText },
                 ],
                 inputText: "",
+                loadingIaReaponse: true,
               }
-            : { ...item };
+            : { ...item, loadingIaReaponse: false };
         return new_item;
       })
     );
+
+    fetch(`http://localhost:8000/model${index}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: data?.inputText }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setModelsState((prevItems: any) =>
+          prevItems.map((item: any, i: number) => {
+            const new_item =
+              i === index
+                ? {
+                    ...item,
+                    mesagges: [
+                      ...item.mesagges,
+                      { sender: "chat", text: data.response },
+                    ],
+                    inputText: "",
+                    loadingIaReaponse: false,
+                  }
+                : { ...item, loadingIaReaponse: false };
+            return new_item;
+          })
+        );
+      })
+      .catch((err) => console.error("Error al obtener datos:", err));
   };
 
   const handleSelectChange = (e: any) => {
@@ -41,8 +78,18 @@ const MessageArea = ({ onSendMessage, ModelsState, setModelsState }: any) => {
       prevItems.map((item: any, i: number) => {
         const new_item =
           i === index
-            ? { ...item, selected: true }
-            : { ...item, selected: false };
+            ? {
+                ...item,
+                loadingIaReaponse: false,
+                selected: true,
+                inputText: "",
+              }
+            : {
+                ...item,
+                loadingIaReaponse: false,
+                selected: false,
+                inputText: "",
+              };
         return new_item;
       })
     );
@@ -60,8 +107,8 @@ const MessageArea = ({ onSendMessage, ModelsState, setModelsState }: any) => {
       prevItems.map((item: any, i: number) => {
         const new_item =
           i === index
-            ? { ...item, inputText: value }
-            : { ...item, inputText: value };
+            ? { ...item, loadingIaReaponse: false, inputText: value }
+            : { ...item, loadingIaReaponse: false, inputText: "" };
         return new_item;
       })
     ); //setSelected(value);
@@ -107,7 +154,7 @@ const MessageArea = ({ onSendMessage, ModelsState, setModelsState }: any) => {
           type="text"
           value={ModelsState?.find((item: any) => item.selected)?.inputText}
           onChange={handleInputChange}
-          placeholder="Escribe un mensaje..."
+          placeholder={modelSelected?.placeholder}
           className="w-full p-2 border border-gray-300 rounded-md"
         />
         <button type="submit" className="p-2 bg-blue-500 text-white rounded-md">
@@ -117,13 +164,20 @@ const MessageArea = ({ onSendMessage, ModelsState, setModelsState }: any) => {
     </form>
   );
 };
-
+const LoadingText = () => {
+  return (
+    <div className="text-center text-gray-700 text-lg font-medium">
+      Cargando<span className="animate-pulse">...</span>
+    </div>
+  );
+};
 //export default MessageArea;
 
-const ChatWindow = ({ messages }: any) => {
+const ChatWindowV0 = ({ ModelsState }: any) => {
+  const modelSelected = ModelsState?.find((item: any) => item.selected);
   return (
     <div className="w-full chat-window p-4 bg-white rounded-lg shadow-lg ">
-      {messages.map((msg: any, index: number) => (
+      {modelSelected?.mesagges.map((msg: any, index: number) => (
         <div
           key={index}
           className={`message ${
@@ -133,6 +187,49 @@ const ChatWindow = ({ messages }: any) => {
           {msg.text}
         </div>
       ))}
+      {modelSelected?.loadingIaReaponse ? <LoadingText /> : null}
+    </div>
+  );
+};
+const ChatWindow = ({ ModelsState }: any) => {
+  const modelSelected = ModelsState?.find((item: any) => item.selected);
+
+  return (
+    <div className="w-full chat-window p-4 bg-white rounded-lg shadow-lg">
+      {modelSelected?.mesagges.map((msg: any, index: number) => {
+        const isUser = msg.sender === "user";
+        const isChat = msg.sender === "chat";
+
+        return (
+          <div
+            key={index}
+            className={`flex items-start my-5 ${
+              isUser ? "justify-end" : "justify-start"
+            }`}
+          >
+            {/* Icono de robot a la izquierda */}
+            {isChat && (
+              <FaRobot className="w-6 h-6 text-purple-500 mr-2 mt-1" />
+            )}
+
+            {/* Mensaje */}
+            <div
+              className={`max-w-2xl p-3 rounded-lg shadow-sm ${
+                isUser ? "bg-blue-100 text-right" : "bg-gray-100"
+              }`}
+            >
+              {msg.text}
+            </div>
+
+            {/* Icono de usuario a la derecha */}
+            {isUser && (
+              <FaUserSecret className="w-6 h-6 text-blue-600 ml-2 mt-1" />
+            )}
+          </div>
+        );
+      })}
+
+      {modelSelected?.loadingIaReaponse && <LoadingText />}
     </div>
   );
 };
@@ -147,8 +244,11 @@ export function Welcome() {
       description:
         "Modelo que a través de un input de entrada intenta predecir el texto que sigue a continuación.",
       name: "Chart Model",
+      placeholder:
+        "Por favor introduce el texto que quieres que el modelo contiue.",
       inputText: "",
       selected: false,
+      loadingIaReaponse: false,
       mesagges: [],
     },
     {
@@ -156,8 +256,11 @@ export function Welcome() {
       description:
         "Modelo que a través de un input de entrada intenta clasificarlo.",
       name: "Sentiments Model",
+      placeholder:
+        "Por favor introduce el texto que quieres que el modelo clasifique.",
       inputText: "",
       selected: false,
+      loadingIaReaponse: false,
       mesagges: [],
     },
     {
@@ -165,8 +268,10 @@ export function Welcome() {
       description:
         "Modelo Gp2 al que se le a aplicado un Fine Tuning para que de mas peso unos textos concretos de una tematica.",
       name: "Gp2 Fine Tuning Model",
+      placeholder: "En que podemos ayudarte.",
       inputText: "",
       selected: false,
+      loadingIaReaponse: false,
       mesagges: [],
     },
   ]);
@@ -192,12 +297,13 @@ export function Welcome() {
         </p>
       </h2>
       <ChatWindow
-        messages={
+        ModelsState={ModelsState}
+        /* messages={
           ModelsState?.find((item: any) => item.selected)?.mesagges || []
-          /* [ { sender: "user", text: "text1" },
-          { sender: "chat", text: "messages2" },
-        ] */
-        }
+        } */
+        /* [ { sender: "user", text: "text1" },
+        { sender: "chat", text: "messages2" },
+      ] */
       />
       <MessageArea
         ModelsState={ModelsState}
